358e347927646145adbe4d1820e13b8d6866c305
JIMIN1020 <jimins4920@gmail.com>
2023-12-27 21:50:35 +0900

    redux-persist 연동

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,33 @@
 import { configureStore } from "@reduxjs/toolkit";
+import {
+  persistReducer,
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import localStorage from "redux-persist/lib/storage";
 import rootReducer from "./reducer";
 
+const persistConfig = {
+  key: "root",
+  storage: localStorage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 const store = configureStore({
-  reducer: rootReducer,
+  reducer: persistedReducer,
   // redux toolkit의 기본 middleware 가져오기 -> 이후에 다른 middleware를 끼워넣기 위해서
   middleware: (getDefaultMiddleware) => {
     const defaultMiddleware = getDefaultMiddleware();
     return [...defaultMiddleware];
   },
 });
+
+export const persistor = persistStore(store);
 
 export default store;
